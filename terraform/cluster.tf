2655603d22417180b6339259e06bc25b10804ade
jones2026 <jones2026@gmail.com>
2021-01-18 12:42:09 -0600

    fix: increase cluster size

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -8,7 +8,7 @@ data "google_client_config" "provider" {}
 resource "google_container_cluster" "primary" {
   name               = "my-gke-cluster"
   location           = "us-central1-c"
-  initial_node_count = 1
+  initial_node_count = 3
   node_config {
     preemptible = true
   }
