92abb74acca45704680e9080cb1bf56109d1efaf
jones2026 <jones2026@gmail.com>
2021-01-18 00:11:50 -0600

    feat: add in statefile bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,20 @@
+terraform {
+  backend "gcs" {
+    bucket = "jones2026-tf-state"
+    prefix = "fun-with-k8s"
+  }
+}
+
 provider "google" {
-  project     = "fun-with-k8s-301821"
-  region      = "us-central1"
+  project = "fun-with-k8s-301821"
+  region  = "us-central1"
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "my-gke-cluster"
-  location = "us-central1"
-  initial_node_count       = 1
+  name               = "my-gke-cluster"
+  location           = "us-central1"
+  initial_node_count = 1
+  node_config {
+    preemptible = true
+  }
 }
