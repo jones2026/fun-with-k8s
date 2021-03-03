60789379693924b21e4e30e2cbfe3751b771b6e9
jones2026 <jones2026@gmail.com>
2021-03-02 23:47:38 -0600

    update chi

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/jones2026/fun-with-k8s
 
-go 1.15
+go 1.16
 
-require github.com/go-chi/chi v1.5.4
+require github.com/go-chi/chi/v5 v5.0.0
--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jones2026/fun-with-k8s/handlers"
 )
 
